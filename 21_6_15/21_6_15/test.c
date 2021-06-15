0a95d67031f6495fefa3cb2d1a64689de3709135
Yangw507 <1667223284@qq.com>
2021-06-15 21:52:54 +0800

    6.15 code

---
+++ 21_6_15/21_6_15/test.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d",&num);
+//	if (num % 2 == 1)
+//	{
+//		printf("%d是基数\n",num);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//		if (i % 2 == 1)
+//		{
+//			printf("%d\n", i);
+//		}
+//		i++;
+//	}
+//}
+////高效的判断基数方法
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//		printf("%d\n", i);
+//		i += 2;
+//	}
+//	return 0;
+//}
+//输出星期几
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d\n", &day);
+//
+//	switch (day)
+//	{
+//	case 1:
+//	{
+//		printf("1");
+//		break;
+//	}
+//	case 2:
+//	{
+//		printf("2");
+//		break;
+//	}
+//		//....
+//	}
+//
+//	return 0;
+//}
+// 打印0-9
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch>'9')
+//		{
+//			continue;
+//		}
+//		putchar(ch);
+//	}
+//	return 0;
+//}
