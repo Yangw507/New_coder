adf28ca4f8e5fa5966e31983b979c5f11b3b5d16
Yangw507 <85477234+Yangw507@users.noreply.github.com>
2021-09-11 12:09:40 +0100

    Add files via upload

---
+++ test_21_9_6/test_21_9_6/test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int b = 2;
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n",+ - a);
+//	b++;
+//	printf("b = %d\n", b);
+//	return 0;
+//}
---
+++ test_21_9_6/test_21_9_6/test2.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+extern int b;
+int main()
+{
+	printf("b = %d\n", b);
+	return 0;
+}
