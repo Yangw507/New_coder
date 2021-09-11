28e2e48044af3136df1df72ae88ecc8980104314
Yangw507 <85477234+Yangw507@users.noreply.github.com>
2021-09-11 18:14:15 +0100

    Add files via upload
    getchar() and putchar

---
+++ test_21_9_11/test_21_9_11/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	//judge cardinal number or even number.
+//	if (num % 2 == 1)
+//	{
+//		printf("%d is cardinal number!", num);
+//	}
+//	else
+//	{
+//		printf("%d is even number!", num);
+//	}
+//	return 0;
+//}
+int main()
+{
+	int ch = 0;
+	while ((ch = getchar()) != EOF)//input ctrl+z to stop!
+	{
+		putchar(ch);
+
+	}
+	return 0;
+}
